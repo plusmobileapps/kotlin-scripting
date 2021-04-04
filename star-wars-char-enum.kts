ad463cfb2dcdbefa432863b4bddf21a48a69d168
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-04 16:57:49 -0700

    create project skeleton

--- star-wars-char-enum.kts
+++ star-wars-char-enum.kts
@@ -1,3 +1,14 @@
 #!/usr/bin/env kscript
 
+@file:MavenRepository("kotlinx", "https://kotlin.bintray.com/kotlinx/")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.1")
+@file:DependsOn("com.github.doyaaaaaken:kotlin-csv-jvm:0.15.1")
+@file:Include("Main.kt")
+@file:Include("Shell.kt")
+@file:Include("CsvReader.kt")
+@file:Include("Logger.kt")
+@file:Include("MyArgs.kt")
+@file:Include("EnumGenerator.kt")
+@file:Include("FileWriter.kt")
+
 println("Hello from Kotlin!")
