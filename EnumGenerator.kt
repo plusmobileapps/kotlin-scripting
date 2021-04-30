d631424a73e3bacf31d35a881dc537403ff0d242
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-30 00:09:06 -0700

    write a couple more tests

--- EnumGenerator.kt
+++ EnumGenerator.kt
@@ -19,8 +19,9 @@ class EnumGenerator(private val logger: Logger) {
     fun processRow(row: Map<String, String>) {
         logger.verbose("Processing row: $row")
         val enumName = row["name"].orEmpty()
-            .split("(-)".toRegex()) //split name into words
+            .split("(-)|( )".toRegex()) //split name into words
             .joinToString(separator = "_", transform = String::toUpperCase) //capitalize and join words with _
+            .replace("É", "E")
         content += """    $enumName("${row["name"]}", "${row["homeworld"]}", "${row["species"]}"),"""
         content += "\n"
     }
--- StarWarsCharacters.kt
+++ StarWarsCharacters.kt
@@ -1,176 +1,92 @@
 //this file is auto-generated
 enum class StarWarsCharacter(val name: String, val homeworld: String, val species: String) {
-LUKE _SKYWALKER(Luke Skywalker, Tatooine, Human), 
 
-C-3_PO(C-3PO, Tatooine, Droid), 
+    LUKE_SKYWALKER("Luke Skywalker", "Tatooine", "Human"),
+    C_3PO("C-3PO", "Tatooine", "Droid"),
+    R2_D2("R2-D2", "Naboo", "Droid"),
+    DARTH_VADER("Darth Vader", "Tatooine", "Human"),
+    LEIA_ORGANA("Leia Organa", "Alderaan", "Human"),
+    OWEN_LARS("Owen Lars", "Tatooine", "Human"),
+    BERU_WHITESUN_LARS("Beru Whitesun lars", "Tatooine", "Human"),
+    R5_D4("R5-D4", "Tatooine", "Droid"),
+    BIGGS_DARKLIGHTER("Biggs Darklighter", "Tatooine", "Human"),
+    OBI_WAN_KENOBI("Obi-Wan Kenobi", "Stewjon", "Human"),
+    ANAKIN_SKYWALKER("Anakin Skywalker", "Tatooine", "Human"),
+    WILHUFF_TARKIN("Wilhuff Tarkin", "Eriadu", "Human"),
+    CHEWBACCA("Chewbacca", "Kashyyyk", "Wookiee"),
+    HAN_SOLO("Han Solo", "Corellia", "Human"),
+    GREEDO("Greedo", "Rodia", "Rodian"),
+    JABBA_DESILIJIC_TIURE("Jabba Desilijic Tiure", "Nal Hutta", "Hutt"),
+    WEDGE_ANTILLES("Wedge Antilles", "Corellia", "Human"),
+    JEK_TONO_PORKINS("Jek Tono Porkins", "Bestine IV", "Human"),
+    YODA("Yoda", "NA", "Yoda's species"),
+    PALPATINE("Palpatine", "Naboo", "Human"),
+    BOBA_FETT("Boba Fett", "Kamino", "Human"),
+    IG_88("IG-88", "NA", "Droid"),
+    BOSSK("Bossk", "Trandosha", "Trandoshan"),
+    LANDO_CALRISSIAN("Lando Calrissian", "Socorro", "Human"),
+    LOBOT("Lobot", "Bespin", "Human"),
+    ACKBAR("Ackbar", "Mon Cala", "Mon Calamari"),
+    MON_MOTHMA("Mon Mothma", "Chandrila", "Human"),
+    ARVEL_CRYNYD("Arvel Crynyd", "NA", "Human"),
+    WICKET_SYSTRI_WARRICK("Wicket Systri Warrick", "Endor", "Ewok"),
+    NIEN_NUNB("Nien Nunb", "Sullust", "Sullustan"),
+    QUI_GON_JINN("Qui-Gon Jinn", "NA", "Human"),
+    NUTE_GUNRAY("Nute Gunray", "Cato Neimoidia", "Neimodian"),
+    FINIS_VALORUM("Finis Valorum", "Coruscant", "Human"),
+    JAR_JAR_BINKS("Jar Jar Binks", "Naboo", "Gungan"),
+    ROOS_TARPALS("Roos Tarpals", "Naboo", "Gungan"),
+    RUGOR_NASS("Rugor Nass", "Naboo", "Gungan"),
+    RIC_OLIE("Ric Olié", "Naboo", "NA"),
+    WATTO("Watto", "Toydaria", "Toydarian"),
+    SEBULBA("Sebulba", "Malastare", "Dug"),
+    QUARSH_PANAKA("Quarsh Panaka", "Naboo", "NA"),
+    SHMI_SKYWALKER("Shmi Skywalker", "Tatooine", "Human"),
+    DARTH_MAUL("Darth Maul", "Dathomir", "Zabrak"),
+    BIB_FORTUNA("Bib Fortuna", "Ryloth", "Twi'lek"),
+    AYLA_SECURA("Ayla Secura", "Ryloth", "Twi'lek"),
+    DUD_BOLT("Dud Bolt", "Vulpter", "Vulptereen"),
+    GASGANO("Gasgano", "Troiken", "Xexto"),
+    BEN_QUADINAROS("Ben Quadinaros", "Tund", "Toong"),
+    MACE_WINDU("Mace Windu", "Haruun Kal", "Human"),
+    KI_ADI_MUNDI("Ki-Adi-Mundi", "Cerea", "Cerean"),
+    KIT_FISTO("Kit Fisto", "Glee Anselm", "Nautolan"),
+    EETH_KOTH("Eeth Koth", "Iridonia", "Zabrak"),
+    ADI_GALLIA("Adi Gallia", "Coruscant", "Tholothian"),
+    SAESEE_TIIN("Saesee Tiin", "Iktotch", "Iktotchi"),
+    YARAEL_POOF("Yarael Poof", "Quermia", "Quermian"),
+    PLO_KOON("Plo Koon", "Dorin", "Kel Dor"),
+    MAS_AMEDDA("Mas Amedda", "Champala", "Chagrian"),
+    GREGAR_TYPHO("Gregar Typho", "Naboo", "Human"),
+    CORDE("Cordé", "Naboo", "Human"),
+    CLIEGG_LARS("Cliegg Lars", "Tatooine", "Human"),
+    POGGLE_THE_LESSER("Poggle the Lesser", "Geonosis", "Geonosian"),
+    LUMINARA_UNDULI("Luminara Unduli", "Mirial", "Mirialan"),
+    BARRISS_OFFEE("Barriss Offee", "Mirial", "Mirialan"),
+    DORME("Dormé", "Naboo", "Human"),
+    DOOKU("Dooku", "Serenno", "Human"),
+    BAIL_PRESTOR_ORGANA("Bail Prestor Organa", "Alderaan", "Human"),
+    JANGO_FETT("Jango Fett", "Concord Dawn", "Human"),
+    ZAM_WESELL("Zam Wesell", "Zolan", "Clawdite"),
+    DEXTER_JETTSTER("Dexter Jettster", "Ojom", "Besalisk"),
+    LAMA_SU("Lama Su", "Kamino", "Kaminoan"),
+    TAUN_WE("Taun We", "Kamino", "Kaminoan"),
+    JOCASTA_NU("Jocasta Nu", "Coruscant", "Human"),
+    RATTS_TYERELL("Ratts Tyerell", "Aleen Minor", "Aleena"),
+    R4_P17("R4-P17", "NA", "NA"),
+    WAT_TAMBOR("Wat Tambor", "Skako", "Skakoan"),
+    SAN_HILL("San Hill", "Muunilinst", "Muun"),
+    SHAAK_TI("Shaak Ti", "Shili", "Togruta"),
+    GRIEVOUS("Grievous", "Kalee", "Kaleesh"),
+    TARFFUL("Tarfful", "Kashyyyk", "Wookiee"),
+    RAYMUS_ANTILLES("Raymus Antilles", "Alderaan", "Human"),
+    SLY_MOORE("Sly Moore", "Umbara", "NA"),
+    TION_MEDON("Tion Medon", "Utapau", "Pau'an"),
+    FINN("Finn", "NA", "Human"),
+    REY("Rey", "NA", "Human"),
+    POE_DAMERON("Poe Dameron", "NA", "Human"),
+    BB8("BB8", "NA", "Droid"),
+    CAPTAIN_PHASMA("Captain Phasma", "NA", "NA"),
+    PADME_AMIDALA("Padmé Amidala", "Naboo", "Human"),
 
-R2-_D2(R2-D2, Naboo, Droid), 
-
-DARTH _VADER(Darth Vader, Tatooine, Human), 
-
-LEIA _ORGANA(Leia Organa, Alderaan, Human), 
-
-OWEN _LARS(Owen Lars, Tatooine, Human), 
-
-BERU _WHITESUN LARS(Beru Whitesun lars, Tatooine, Human), 
-
-R5-_D4(R5-D4, Tatooine, Droid), 
-
-BIGGS _DARKLIGHTER(Biggs Darklighter, Tatooine, Human), 
-
-OBI-_WAN _KENOBI(Obi-Wan Kenobi, Stewjon, Human), 
-
-ANAKIN _SKYWALKER(Anakin Skywalker, Tatooine, Human), 
-
-WILHUFF _TARKIN(Wilhuff Tarkin, Eriadu, Human), 
-
-CHEWBACCA(Chewbacca, Kashyyyk, Wookiee), 
-
-HAN _SOLO(Han Solo, Corellia, Human), 
-
-GREEDO(Greedo, Rodia, Rodian), 
-
-JABBA _DESILIJIC _TIURE(Jabba Desilijic Tiure, Nal Hutta, Hutt), 
-
-WEDGE _ANTILLES(Wedge Antilles, Corellia, Human), 
-
-JEK _TONO _PORKINS(Jek Tono Porkins, Bestine IV, Human), 
-
-YODA(Yoda, NA, Yoda's species), 
-
-PALPATINE(Palpatine, Naboo, Human), 
-
-BOBA _FETT(Boba Fett, Kamino, Human), 
-
-IG-88(IG-88, NA, Droid), 
-
-BOSSK(Bossk, Trandosha, Trandoshan), 
-
-LANDO _CALRISSIAN(Lando Calrissian, Socorro, Human), 
-
-LOBOT(Lobot, Bespin, Human), 
-
-ACKBAR(Ackbar, Mon Cala, Mon Calamari), 
-
-MON _MOTHMA(Mon Mothma, Chandrila, Human), 
-
-ARVEL _CRYNYD(Arvel Crynyd, NA, Human), 
-
-WICKET _SYSTRI _WARRICK(Wicket Systri Warrick, Endor, Ewok), 
-
-NIEN _NUNB(Nien Nunb, Sullust, Sullustan), 
-
-QUI-_GON _JINN(Qui-Gon Jinn, NA, Human), 
-
-NUTE _GUNRAY(Nute Gunray, Cato Neimoidia, Neimodian), 
-
-FINIS _VALORUM(Finis Valorum, Coruscant, Human), 
-
-JAR _JAR _BINKS(Jar Jar Binks, Naboo, Gungan), 
-
-ROOS _TARPALS(Roos Tarpals, Naboo, Gungan), 
-
-RUGOR _NASS(Rugor Nass, Naboo, Gungan), 
-
-RIC _OLIÉ(Ric Olié, Naboo, NA), 
-
-WATTO(Watto, Toydaria, Toydarian), 
-
-SEBULBA(Sebulba, Malastare, Dug), 
-
-QUARSH _PANAKA(Quarsh Panaka, Naboo, NA), 
-
-SHMI _SKYWALKER(Shmi Skywalker, Tatooine, Human), 
-
-DARTH _MAUL(Darth Maul, Dathomir, Zabrak), 
-
-BIB _FORTUNA(Bib Fortuna, Ryloth, Twi'lek), 
-
-AYLA _SECURA(Ayla Secura, Ryloth, Twi'lek), 
-
-DUD _BOLT(Dud Bolt, Vulpter, Vulptereen), 
-
-GASGANO(Gasgano, Troiken, Xexto), 
-
-BEN _QUADINAROS(Ben Quadinaros, Tund, Toong), 
-
-MACE _WINDU(Mace Windu, Haruun Kal, Human), 
-
-KI-_ADI-_MUNDI(Ki-Adi-Mundi, Cerea, Cerean), 
-
-KIT _FISTO(Kit Fisto, Glee Anselm, Nautolan), 
-
-EETH _KOTH(Eeth Koth, Iridonia, Zabrak), 
-
-ADI _GALLIA(Adi Gallia, Coruscant, Tholothian), 
-
-SAESEE _TIIN(Saesee Tiin, Iktotch, Iktotchi), 
-
-YARAEL _POOF(Yarael Poof, Quermia, Quermian), 
-
-PLO _KOON(Plo Koon, Dorin, Kel Dor), 
-
-MAS _AMEDDA(Mas Amedda, Champala, Chagrian), 
-
-GREGAR _TYPHO(Gregar Typho, Naboo, Human), 
-
-CORDÉ(Cordé, Naboo, Human), 
-
-CLIEGG _LARS(Cliegg Lars, Tatooine, Human), 
-
-POGGLE THE _LESSER(Poggle the Lesser, Geonosis, Geonosian), 
-
-LUMINARA _UNDULI(Luminara Unduli, Mirial, Mirialan), 
-
-BARRISS _OFFEE(Barriss Offee, Mirial, Mirialan), 
-
-DORMÉ(Dormé, Naboo, Human), 
-
-DOOKU(Dooku, Serenno, Human), 
-
-BAIL _PRESTOR _ORGANA(Bail Prestor Organa, Alderaan, Human), 
-
-JANGO _FETT(Jango Fett, Concord Dawn, Human), 
-
-ZAM _WESELL(Zam Wesell, Zolan, Clawdite), 
-
-DEXTER _JETTSTER(Dexter Jettster, Ojom, Besalisk), 
-
-LAMA _SU(Lama Su, Kamino, Kaminoan), 
-
-TAUN _WE(Taun We, Kamino, Kaminoan), 
-
-JOCASTA _NU(Jocasta Nu, Coruscant, Human), 
-
-RATTS _TYERELL(Ratts Tyerell, Aleen Minor, Aleena), 
-
-R4-_P17(R4-P17, NA, NA), 
-
-WAT _TAMBOR(Wat Tambor, Skako, Skakoan), 
-
-SAN _HILL(San Hill, Muunilinst, Muun), 
-
-SHAAK _TI(Shaak Ti, Shili, Togruta), 
-
-GRIEVOUS(Grievous, Kalee, Kaleesh), 
-
-TARFFUL(Tarfful, Kashyyyk, Wookiee), 
-
-RAYMUS _ANTILLES(Raymus Antilles, Alderaan, Human), 
-
-SLY _MOORE(Sly Moore, Umbara, NA), 
-
-TION _MEDON(Tion Medon, Utapau, Pau'an), 
-
-FINN(Finn, NA, Human), 
-
-REY(Rey, NA, Human), 
-
-POE _DAMERON(Poe Dameron, NA, Human), 
-
-BB8(BB8, NA, Droid), 
-
-CAPTAIN _PHASMA(Captain Phasma, NA, NA), 
-
-PADMÉ _AMIDALA(Padmé Amidala, Naboo, Human), 
 }
